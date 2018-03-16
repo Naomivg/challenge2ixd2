fefd77e0545af2a2d5e9d0a32cd6186bdbb3f727
Naomivg <36531933+Naomivg@users.noreply.github.com>
2018-03-16 09:31:17 +0100

    working clock

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,12 +1,28 @@
+
+/* V1;
 window.onload = function clock(){
 	var time = new Date();
     var hours = time.getHours();
     var minutes = time.getMinutes();
     var seconds = time.getSeconds();
     document.getElementById("clock").innerHTML = 
-    (hours + ":" + minutes + ":" + seconds);setInterval('clock()', 1000);
+    (hours + ":" + minutes + ":" + seconds);
 }
+*/
 
-
+window.onload = function() {
+	function clock(){
+		var time = new Date();
+	    var hours = time.getHours();
+	    var minutes = time.getMinutes();
+	    var seconds = time.getSeconds();
+	    document.getElementById("clock").innerHTML = 
+	    (hours + ":" + minutes + ":" + seconds);
+	}
+	clock();
+	setInterval(function(){
+		clock();
+	}, 1000);
+}
 
 
