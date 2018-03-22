435e3500dd8d731494dde56e4bfa6ae5cd274bf8
Naomivg <36531933+Naomivg@users.noreply.github.com>
2018-03-22 19:51:44 +0100

    fixed stars and cleaned css and js

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,26 +1,13 @@
-/* V1;
-window.onload = function clock(){
-var time = new Date();
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
-    document.getElementById("clock").innerHTML = (hours + ":" + minutes + ":" + seconds);
-}
-*/
-
-/*
-Getting time
-*/
+//Getting time
 window.onload = function() {
  function clock(){
   var time = new Date();
   var hours = time.getHours();
   var minutes = time.getMinutes();
   var seconds = time.getSeconds();
-  minutes = ("0" + time.getMinutes()).slice(-2);
   seconds = ("0" + time.getSeconds()).slice(-2);
   document.getElementById("clock").innerHTML = (hours + ":" + minutes + ":" + seconds);
-   switch(15){
+   switch(hours){
     case 0:
     case 1:
     case 2:
@@ -46,6 +33,7 @@ window.onload = function() {
 	case 12:
 	removeSun();
 	animatedTextMorning();
+	animationBird();
 	morning();
 	break;
 	case 13:
@@ -62,111 +50,110 @@ window.onload = function() {
 	break;
 	}
 }
-	/*retrieving clock*/
+//retrieving clock
+clock();
+setInterval(function(){
 	clock();
-	setInterval(function(){
-		clock();
-	}, 1000);
-/*morning*/
-	function morning(){
-		var clockColor = document.getElementById("clock");
-		var textColorClock = new TimelineMax();
-		textColorClock.to(clockColor, 1, {color:"#000000", ease:Power3.easeOut});
-		//TweenLite.to('#clock', 1, {color:"#000000", ease:Power3.easeOut});
-		TweenLite.to("body", 3, {backgroundImage:"linear-gradient(to right, #FB903F, #FBD63F, #FB903F)"});
+}, 1000);
 
-		var animatedBird = document.getElementsByClassName("bird");
-		var birdy = new TimelineMax({repeat:-1});
-		birdy.to(animatedBird, 5, {left:window.innerWidth, yoyo:false, ease:Linear.easeNone});
-		birdy.restart();
+//morning colors background and text
+function morning(){
+	TweenMax.to('#clock', 1, {color:"#000000", ease:Power3.easeOut});
+	TweenMax.to("body", 3, {backgroundImage:"linear-gradient(to top, #FBD63F, #FB903F)"});
+}
+
+//afternoon
+function afternoon(){		
+	TweenMax.to("body", 3, {backgroundImage:"linear-gradient(to top, #99DBFF, #6ABBFF"});
+	TweenMax.to("#clock", 1, {color:"#000000", ease:Power3.easeOut});
+}
+
+//night
+function darkness(){
+	TweenMax.to("body", 3, {backgroundImage:"linear-gradient(to top, #0A006A, #000000)"});
+	TweenMax.to("#clock", 1, {color:"#ffffff", ease:Power3.easeOut});
+}
+
+function animatedSun(){
+	var risingSun = document.getElementsByClassName("sun");
+	var sunny = new TimelineMax({ yoyo:true});
+	sunny.to(risingSun, 1, {bezier:{type:"soft", 
+		values:[{x:0, y:0} , 
+		{x:50, y:-50} , 
+		{x:100, y:-100} , 
+		{x:150, y:-150} , 
+		{x:200, y:-200} ,
+		{x:250, y:-250} ,
+		{x:300, y:-200} ,
+		{x:350, y:-200} ,
+		{x:400, y:-150} ,
+		{x:450, y:-100} ,
+		{x:500, y:-50}  ,
+		{x:550, y:0} ]}, ease:Power3.easeInOut});
+}
+
+function removeSun(){
+	if(document.querySelector(".sun") !== null) {
+		var shine = document.getElementsByClassName("sun")[0];
+		shine.remove();
+	} else {
 	}
+}
 
-	/*afternoon*/
-	function afternoon(){		
-		TweenMax.to("body", 3, {backgroundImage:"linear-gradient(to right, #6ABBFF, #99DBFF, #6ABBFF)"});
-
-		var colorText = document.getElementById("clock");
-		var textColorAfternoon = new TimelineMax({repeat:0});
-		textColorAfternoon.to(colorText, 1, {color:"#000000", ease:Power3.easeOut});
+function removeBird(){
+	if(document.querySelector(".bird") !== null) {
+		var bird = document.getElementsByClassName("bird")[0];
+		bird.remove();
+	} else {
 	}
+}
 
-	/*night*/
-	function darkness(){
-		TweenMax.to("body", 3, {backgroundImage:"linear-gradient(to right, #000000, #0A006A, #000000)"});
-		TweenMax.to("#clock", 1, {color:"#ffffff", ease:Power3.easeOut});
-	}
+function animatedTextMorning(){
+	TweenMax.to(".text", 1, {text:"good morning"});
+	TweenMax.to(".degrees", 1, {text:"It is now 10 degrees"});
+}
 
-	function animatedSun(){
-	//TweenMax.to("#sun", 5, {rotation: -90 , transformOrigin:"10px -250px", ease:Linear.easeNone});
-		var risingSun = document.getElementsByClassName("sun");
-		var sunny = new TimelineMax({ yoyo:true});
-		//sunny.to(risingSun, 1, {bezier:[{x:0, y:0}, {x:250, y:-90}, {x:0, y:0}], ease:Power3.easeInOut});
-		sunny.to(risingSun, 1, {bezier:{type:"soft", 
-			values:[{x:0, y:0} , 
-			{x:50, y:-50} , 
-			{x:100, y:-100} , 
-			{x:150, y:-150} , 
-			{x:200, y:-200} ,
-			{x:250, y:-250} ,
-			{x:300, y:-200} ,
-			{x:350, y:-200} ,
-			{x:400, y:-150} ,
-			{x:450, y:-100} ,
-			{x:500, y:-50}  ,
-			{x:550, y:0} ]}, ease:Power3.easeInOut});
-	}
+function animatedTextAfternoon(){
+	TweenMax.to(".text", 1, {text:"good afternoon"});
+	TweenMax.to(".degrees", 1, {text:"It is now 19 degrees"});
+}
 
-	function removeSun(){
-		if(document.querySelector(".sun") !== null) {
-			var shine = document.getElementsByClassName("sun")[0];
-			shine.remove();
-		} else {
-		}
-	}
+function animatedTextEvening(){
+	TweenMax.to(".text", 1, {text:"good night"});
+	TweenMax.to(".text", 1, {color:"#ffffff", ease:Power3.easeOut});
+	TweenMax.to(".degrees", 1, {text:"It is now 5 degrees"});
+	TweenMax.to(".degrees", 1, {color:"#ffffff", ease:Power3.easeOut});
+}
 
-	function removeBird(){
-		if(document.querySelector(".bird") !== null) {
-			var bird = document.getElementsByClassName("bird")[0];
-			bird.remove();
-		} else {
-		}
-	}
+function animationBird(){
+	var animatedBird = document.getElementsByClassName("bird");
+	var birdy = new TimelineMax({repeat:-1});
+	birdy.to(animatedBird, 5, {left:window.innerWidth, yoyo:false, ease:Linear.easeNone});
+	birdy.restart();
+}
 
-	function animatedTextAfternoon(){
-		TweenLite.to(".text", 1, {text:"good afternoon"});
-	}
-
-	function animatedTextMorning(){
-		TweenLite.to(".text", 1, {text:"good morning"});
-	}
-
-	function animatedTextEvening(){
-		TweenLite.to(".text", 1, {text:"good night"});
-		TweenLite.to(".text", 1, {color:"#ffffff", ease:Power3.easeOut});
-	}
-
-	function animatedStars(){
-	var total = 5;
+function animatedStars(){
+	var numberStars = 20;
 	var container = document.getElementById("container");
-	var w = container.offsetWidth; 
-	var h = container.offsetHeight;
+	var w = window.innerWidth;
+	var h = window.innerWidth;
+	var starry = new TimelineMax({repeat:-1});
 	 
-	 for (var i=0 , div ; i<total; i++){ 
+	for (var i=1 , div ; i<numberStars; i++){ 
 	  div = document.createElement("div");   
 	  div.className="dot";
 	  container.appendChild(div);
-	  TweenMax.set(div,{x:execute(0,w),y:execute(-100,100),opacity:1,scale:execute(0,0.5)+0.5,});
+	  starry.set(div,{x:execute(0,w),y:execute(-600,600),opacity:1,scale:execute(0,0.5)+0.5,});
 	  animateDots(div);
-	 }
+ 	}
 	 
-	 function animateDots(star){   
-	 TweenMax.to(star,execute(0,5)+3,{y:h,ease:Linear.easeNone,repeat:-1, delay:-5});
-	 TweenMax.to(star,execute(0,5)+1,{x:"+=70", repeat:-1,yoyo:true,ease:Power3.easeInOut});
-	 TweenMax.to(star,execute(0,1)+0.5,{opacity:0, repeat:-1,yoyo:true,ease:Power3.easeInOut});
-	 }
+	function animateDots(star){   
+	 TweenMax.to(star,execute(0,1)+0.5,{opacity:0,yoyo:true,ease:Power3.easeInOut});
+	}
 
 	function execute(min, max){ 
-    return min + ( Math.random() * (max - min)); 
-  }
-	}
+		return min + ( Math.random() * (max - min)); 
+  	}
+}
 };
+
